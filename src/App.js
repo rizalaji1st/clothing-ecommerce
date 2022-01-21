e480ab0e94119b026b7064fb7ac824ab5ce0e331
rizalaji1st <rizalaji561@gmail.com>
2022-01-21 11:23:49 +0700

    Menambahkan pages sign in beserta componentnya

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <Header/>
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Route path='/shop' component={ShopPage} />
+      <Route path='/sign-in' component={SignInAndSignUpPage} />
     </Switch>
   </div>;
 }
